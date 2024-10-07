79cb6fa8325af1a8a4f17522e8f723f4a07d2771
Ryoichiro Oka <ryoka0418@gmail.com>
2024-10-07 22:11:59 +0900

    update build stuff

--- Setup (run before opening solution).bat
+++ Setup (run before opening solution).bat
@@ -1,8 +1,20 @@
 :: This script creates a symlink to the game binaries to account for different installation directories on different systems.
 
-dpath="C:\torch-server"
+@echo off
+set /p path="Please enter the folder location of your SpaceEngineersDedicated.exe: "
 cd %~dp0
-mklink /J TorchBinaries %dpath%
+mklink /J GameBinaries "%path%"
+if errorlevel 1 goto Error
+echo Done!
+goto End
+:Error
+echo An error occured creating the symlink.
+goto EndFinal
+:End
+
+set /p path="Please enter the folder location of your Torch.Server.exe: "
+cd %~dp0
+mklink /J TorchBinaries "%path%"
 if errorlevel 1 goto Error
 echo Done! You can now open the Torch solution without issue.
 goto EndFinal
